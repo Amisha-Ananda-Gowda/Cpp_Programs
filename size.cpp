d080d4ca386799d0321617d07b3996bf4e47df75
Amisha <21j03.amisha@sjec.ac.in>
2024-05-26 20:38:28 +0530

    Added new programs

---
+++ size.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a;
+    float b;
+    char c;
+    bool d;
+    long int e;
+    short int f;
+    cout<<"Size of int is "<<sizeof(a)<<" bytes"<<endl;
+    cout<<"Size of float is "<<sizeof(b)<<" bytes"<<endl;
+    cout<<"Size of char is "<<sizeof(c)<<" byte"<<endl;
+    cout<<"Size of bool is "<<sizeof(d)<<" byte"<<endl;
+    cout<<"Size of long int is "<<sizeof(e)<<" bytes"<<endl;
+    cout<<"Size of short int is "<<sizeof(f)<<" bytes"<<endl;
+}
---
+++ sum.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n1,n2;
+    cout<<"Enter first number: "<<endl;
+    cin>>n1;
+    cout<<"Enter second number: "<<endl;
+    cin>>n2;
+    int sum=n1+n2;
+    cout<<"Sum of "<<n1<<" and "<<n2<<" = "<<sum<<endl;
+    return 0;
+}
