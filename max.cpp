4e678084f2a4bf0e7ce4f8c125682ec0e1aa2eb3
Amisha <21j03.amisha@sjec.ac.in>
2024-05-28 19:06:29 +0530

    Added new program

---
+++ max.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+int a, b, c;
+cout<<"Enter three numbers:"<<endl;
+cin>>a>>b>>c;
+
+if(a>b){
+    if(a>c){
+        cout<<a<<" is greater than "<<b<<" and "<<c<<endl;
+    }
+    else{
+        cout<<c<<" is greater than "<<a<<" and "<<b<<endl;
+    }
+}
+else {
+    if(b>c){
+        cout<<b<<" is greater than "<<a<<" and "<<c<<endl;
+    }
+    else{
+        cout<<c<<" is greater than "<<a<<" and "<<b<<endl;
+    }
+}
+return 0;
+}
