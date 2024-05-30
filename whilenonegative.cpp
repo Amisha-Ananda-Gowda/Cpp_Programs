fbd85ba6d140b50caf51633e96c03ed5f70e701d
Amisha <21j03.amisha@sjec.ac.in>
2024-05-30 21:53:15 +0530

    Added new programs

---
+++ whilenonegative.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cout<<"Enter a number"<<endl;
+    cin>>n;
+    while(n>=0){
+        cout<<"Enter a number"<<endl;
+        cin>>n;
+    }
+    
+    return 0;
+}
