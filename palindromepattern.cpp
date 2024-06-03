9dc861aaaf52190faf139d7537777ed880011dae
Amisha <21j03.amisha@sjec.ac.in>
2024-06-03 23:09:22 +0530

    added new programs

--- palindromepattern.cpp
+++ palindromepattern.cpp
@@ -7,10 +7,14 @@ int main(){
     cin>>n;
     for(i=1;i<=n;i++){
         for(j=1;j<=n-i;j++){
-            cout<<" ";
+            cout<<"  ";
         }
-        for(j=1;j<=i;j++){
-            cout<<j<<" ";
+        for(j=1;j<=2*i-1;j++){
+            if(j<i){
+               cout<<i-j+1<<" ";
+            }           
+            else
+            cout<<j-i+1<<" ";
         }
         cout<<endl;
     }
---
+++ starpattern.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int i,j,n;
+    cout<<"Enter a number: "<<endl;
+    cin>>n;
+    return 0;
+}
