eef7832030b9617bf1342434713449fb2b2d2d7d
Amisha <21j03.amisha@sjec.ac.in>
2024-08-29 18:47:49 +0530

    added new programs

---
+++ myname.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int i,j,n;
+    cout<<"Enter the size"<<endl;
+    cin>>n;
+    for(i=1;i<=3*n;i++)
+    {
+        for(j=1;j<=3*n;j++){
+            if((i==1)||(j==1)||(j==3*n)||(i==3*n/2+1)){
+               cout<<" * ";
+            }
+          else{
+            cout<<"   ";
+          }
+        }
+          cout<<endl;
+    }
+for(i=1;i<=n;i++){
+    cout<<endl;
+}
+
+   for(i=1;i<=3*n;i++)
+    {
+        for(j=1;j<=3*n;j++){
+            if((j==1)||(j==3*n)){
+               cout<<" * ";
+            }
+          else{
+            cout<<"   ";
+          }
+        }
+          cout<<endl;
+    }
+
+    return 0;
+}
